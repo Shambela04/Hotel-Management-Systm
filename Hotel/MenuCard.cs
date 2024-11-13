53425ec0c3b6ba9c639497ffc4388fd43071c86e
Shambela Sawant <shantanus871@gmail.com>
2024-11-13 19:18:57 +0530

    Add project files.

---
+++ Hotel/MenuCard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    public class MenuCard
+    {
+        private List<MenuCard> menu = new List<MenuCard>();
+        private List<Orderitm> Orditem = new List<Orderitm>();
+
+        public void DishList()
+        {
+            Console.WriteLine("");
+
+            menu.Add(new MenuCard(1, "Kaju Masala", 100));
+            menu.Add(new MenuCard(2, "Butter Chicken", 220));
+            menu.Add(new MenuCard(3,"Malai kofta", 210));
+            menu.Add(new MenuCard());
+
+        }
+
+    }
+}
---
+++ Hotel/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Hotel
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Hotel resto = new Hotel();
+            int Choose;
+            do
+            {
+
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("WELCOME To The Restaurant...");
+                Console.WriteLine("1. Display Menu :");
+                Console.WriteLine("2. Place Order :");
+                Console.WriteLine("3. View Order :");
+                Console.WriteLine("4. Total Bill :");
+                Console.WriteLine("5. Finish..");
+                Console.WriteLine("-------------------------");
+
+                Choose = int.Parse(Console.ReadLine());
+                switch(Choose)
+                {
+                    case 1:
+                        resto.ShowMenu();
+                        Console.WriteLine("---------");
+                        break;
+
+                    case 2:
+                        resto.placeOrder();
+                        break;
+                        Console.WriteLine("----------");
+                        
+
+                    case 3:
+                        resto.ViewOrder();
+                        break;
+                        Console.WriteLine("----------");
+                        
+
+                    case 4:
+                        resto.TotalBill();
+                        break;
+                        Console.WriteLine("----------");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Exit");
+                        break;
+                        Console.WriteLine("----------");
+                        
+
+                    default:
+                        Console.WriteLine("Enter Valid Option");
+                        break;
+                        Console.WriteLine("--------");
+                }
+
+            }
+
+
+
+
+        }
+
+
+
+    }
+
+    
+
+}
