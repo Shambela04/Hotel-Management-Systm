8a90e725a8a3b5545a0868e36b5b0651601f61f8
ShambelaS <sawantshambela@gmail.com>
2024-11-19 16:34:00 +0530

    Update MenuCard.cs
    Unusable file

--- Hotel/MenuCard.cs
+++ Hotel/MenuCard.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Hotel
+//namespace Hotel
 {
-    public class MenuCard
-    {
-        private List<MenuCard> menu = new List<MenuCard>();
-        private List<Orderitm> Orditem = new List<Orderitm>();
+   // public class MenuCard
+  //  {
+       // private List<MenuCard> menu = new List<MenuCard>();
+        //private List<Orderitm> Orditem = new List<Orderitm>();
 
-        public void DishList()
+       // public void DishList()
         {
-            Console.WriteLine("");
+          //  Console.WriteLine("");
 
-            menu.Add(new MenuCard(1, "Kaju Masala", 100));
-            menu.Add(new MenuCard(2, "Butter Chicken", 220));
-            menu.Add(new MenuCard(3,"Malai kofta", 210));
-            menu.Add(new MenuCard());
+          //  menu.Add(new MenuCard(1, "Kaju Masala", 100));
+          //  menu.Add(new MenuCard(2, "Butter Chicken", 220));
+          //  menu.Add(new MenuCard(3,"Malai kofta", 210));
+          //  menu.Add(new MenuCard());
 
-        }
+       // }
 
-    }
-}
+   // }
+//}
